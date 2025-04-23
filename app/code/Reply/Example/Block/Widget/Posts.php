35f57deb775d4f2625a9ddcb89514f43a5318f70
wilson sgro <w.sgro@bitmama.it>
2025-04-23 09:52:32 +0200

    create widget

---
+++ app/code/Reply/Example/Block/Widget/Posts.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace Reply\Example\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface; 
+ 
+class Posts extends Template implements BlockInterface 
+{
+
+	protected $_template = "widget/posts.phtml";
+}

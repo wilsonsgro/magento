d063edc40b9560ed5070445b4e4de397896d4e95
wilson sgro <w.sgro@bitmama.it>
2025-04-23 09:22:28 +0200

    add new theme example

---
+++ app/design/frontend/Agap2/Default/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @category    Magento 2
+ * @copyright   Copyright (c) 2020 Basiliko (https://www.basiliko.net)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Agap2/Default',
+    __DIR__
+);
